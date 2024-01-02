310731ee8de89570ef102ad747ddf69a92f650d9
EmannuelMartinezTest <rubyroyproductions@gmail.com>
2024-01-02 17:08:58 -0600

    Organize code

--- rps-ui/script.js
+++ rps-ui/script.js
@@ -1,33 +1,37 @@
+// VARIABLES
+
 let choices = document.querySelector(".choices");
 
-let rockNode = document.querySelector(".rock");
-let rock = rockNode.cloneNode(true);
-
-let paperNode = document.querySelector(".paper");
-let paper = paperNode.cloneNode(true);
-
-let scissorsNode = document.querySelector(".scissors");
-let scissors = scissorsNode.cloneNode(true);
-
-let resetButton = document.createElement("button");
-resetButton.textContent = "Play Again?";
-resetButton.className = "reset";
-resetButton.addEventListener("click", resetGame);
+let rock = document.querySelector(".rock").cloneNode(true);
+let paper = document.querySelector(".paper").cloneNode(true);
+let scissors = document.querySelector(".scissors").cloneNode(true);
 
 let userScore = 0;
 let computerScore = 0;
 let rounds = 5;
 
+let uiMessage = document.querySelector(".messages");
+let userScoreMessage = document.querySelector(".userScore");
+let computerScoreMessage = document.querySelector(".computerScore");
+
 let originalChoices = [rock, paper, scissors];
+
+// EVENT LISTENERS
 
 choices.addEventListener("click", playAnimation, false);
 choices.addEventListener("click", game);
 
+// FUNCTIONS
 function game(evt) {
   let userChoice = createCloneByID(evt.target.className);
   let whateverChoice = createCloneByID("rock");
-  let parent = document.querySelector(".choices");
   let computerChoice = createCloneByID(getComputerResults());
+
+  let parent = document.querySelector(".choices");
+
+  let newChildren = [userChoice, whateverChoice, computerChoice];
+
+  let continueGame = true;
 
   userChoice.firstElementChild.className = "";
   userChoice.firstElementChild.classList.add("right");
@@ -35,11 +39,7 @@ function game(evt) {
   computerChoice.firstElementChild.classList.add("left");
   whateverChoice.style.visibility = "hidden";
 
-  let newChildren = [userChoice, whateverChoice, computerChoice];
-
   newChildren.map((button) => (button.disabled = true));
-
-  let continueGame;
 
   setTimeout(() => {
     parent.replaceChildren(...newChildren);
@@ -51,6 +51,41 @@ function game(evt) {
     updateNeutralMessage(continueGame);
     parent.replaceChildren(...originalChoices);
   }, 3300);
+}
+
+// FUNCTIONS -- GAME LOGIC
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return updateUI("tie");
+  }
+  let winner;
+  switch (playerSelection) {
+    case "rock":
+      winner = computerSelection === "paper" ? "computer" : "user";
+      break;
+    case "paper":
+      winner = computerSelection === "scissors" ? "computer" : "user";
+      break;
+    case "scissors":
+      winner = computerSelection === "rock" ? "computer" : "user";
+      break;
+  }
+
+  updateUI(winner, playerSelection, computerSelection);
+}
+// prettier-ignore
+function updateUI(winner, userChoice, computerChoice) {
+  if (winner === "tie") {
+    uiMessage.textContent = `It's a tie!`;
+  }
+  if (winner === "user") {
+    userScoreMessage.textContent = `${++userScore}`;
+    uiMessage.textContent = `You win!\n ${formatText(userChoice)} beats ${formatText(computerChoice)}.`;
+  }
+  if (winner === "computer") {
+    computerScoreMessage.textContent = `${++computerScore}`;
+    uiMessage.textContent = `You lose!\n ${formatText(computerChoice)} beats ${formatText(userChoice)}.`;
+  }
 }
 
 function updateNeutralMessage(continueBool) {
@@ -72,13 +107,13 @@ function displayGameOver() {
   uiMessage.textContent = `Game Over!\n You ${
     userScore > computerScore ? "Win!" : "Lose..."
   }`;
-  uiMessage.appendChild(resetButton);
+  uiMessage.appendChild(createResetButton());
 }
 
 function resetGame() {
   userScore = 0;
   computerScore = 0;
-  uiMessage.removeChild(resetButton);
+  uiMessage.removeChild(uiMessage.lastElementChild);
   uiMessage.textContent = `First to 5 Wins!\nGood luck!`;
   userScoreMessage.textContent = "0";
   computerScoreMessage.textContent = "0";
@@ -86,46 +121,14 @@ function resetGame() {
   choices.addEventListener("click", game);
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return updateUI("tie");
-  }
-  let winner;
-  switch (playerSelection) {
-    case "rock":
-      winner = computerSelection === "paper" ? "computer" : "user";
-      break;
-    case "paper":
-      winner = computerSelection === "scissors" ? "computer" : "user";
-      break;
-    case "scissors":
-      winner = computerSelection === "rock" ? "computer" : "user";
-      break;
-  }
-
-  updateUI(winner, playerSelection, computerSelection);
-}
-
-let uiMessage = document.querySelector(".messages");
-let userScoreMessage = document.querySelector(".userScore");
-let computerScoreMessage = document.querySelector(".computerScore");
-
-function updateUI(winner, userChoice, computerChoice) {
-  if (winner === "tie") {
-    uiMessage.textContent = `It's a tie!`;
-  }
-  if (winner === "user") {
-    userScoreMessage.textContent = `${++userScore}`;
-    uiMessage.textContent = `You win!\n ${formatText(
-      userChoice,
-    )} beats ${formatText(computerChoice)}.`;
-  }
-  if (winner === "computer") {
-    computerScoreMessage.textContent = `${++computerScore}`;
-    uiMessage.textContent = `You lose!\n ${formatText(
-      computerChoice,
-    )} beats ${formatText(userChoice)}.`;
-  }
+// FUNCTIONS -- UTILITY
+function playAnimation() {
+  document.querySelector(".choices").className = "choices";
+  requestAnimationFrame((time) => {
+    requestAnimationFrame((time) => {
+      document.querySelector(".choices").className = "choices animate";
+    });
+  });
 }
 
 function formatText(text) {
@@ -155,11 +158,11 @@ function getComputerResults() {
   }
 }
 
-function playAnimation() {
-  document.querySelector(".choices").className = "choices";
-  requestAnimationFrame((time) => {
-    requestAnimationFrame((time) => {
-      document.querySelector(".choices").className = "choices animate";
-    });
-  });
+function createResetButton() {
+  let resetButton = document.createElement("button");
+  resetButton.textContent = "Play Again?";
+  resetButton.className = "reset";
+  resetButton.addEventListener("click", resetGame);
+
+  return resetButton;
 }
